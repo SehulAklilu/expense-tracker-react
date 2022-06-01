aadc6a50a9450c2a53b0aadf17c08fc0f071a6f3
Sehul Aklilu <asehul@yahoo.com>
2022-06-01 16:53:37 +0300

    Function

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
-
+import { GlobalProvider } from './context/GlobalState';
 import './App.css';
 
 
 
 function App() {
   return (
-    <div >
+    <GlobalProvider >
       <Header />
       <div className='container'>
         <Balance />
@@ -18,7 +18,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
